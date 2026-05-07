1bc9b7f61af2e7b1c58280c8f23b6a61bc7cd9ca
Estebanquito9347 <esteban.ruiz.sec@institutolamerced.edu.ar>
2026-05-07 16:33:44 -0300

    Agrego el sensor

--- temperature.py
+++ temperature.py
@@ -1,4 +1,5 @@
 def is_overheating(temp_c):
     if temp_c < 0:
         raise ValueError("Reading error")
+
     return temp_c > 80
--- test/test_temperaturea.py
+++ test/test_temperaturea.py
@@ -1,1 +1,3 @@
-
+def test_invalid_temperature():
+    with pytest.raises(ValueError):
+        is_overheating(-5)
