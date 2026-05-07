97dfd87c4e8151c19152960f39dc2387d10c209a
Samu-2310 <samuel.gonzalez.sec@institutolamerced.edu.ar>
2026-05-06 21:20:43 -0300

    feat: implementar logica de alarma y validacion de entrada

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+from temperature import is_overheating
+
+temp = float(input("Ingrese temperatura: "))
+
+if is_overheating(temp):
+	print("⚠️ ALARMA: Temperatura alta")
+else:
+	print("✅ Temperatura normal")
 
