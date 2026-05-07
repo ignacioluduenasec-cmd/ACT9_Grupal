127aa4328c3f0d33ac0501c0c3cdb2a57c0c86b6
Estebanquito9347 <esteban.ruiz.sec@institutolamerced.edu.ar>
2026-05-07 16:21:48 -0300

    codigos de alarma basica

--- test/test_temperaturea.py
+++ test/test_temperaturea.py
@@ -1,1 +1,8 @@
+import pytest
+from temperature import is_overheating
 
+def test_high_temperature():
+    assert is_overheating(90) is True
+
+def test_normal_temperature():
+    assert is_overheating(60) is False
