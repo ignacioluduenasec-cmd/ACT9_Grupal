a0b9a5ec6653672cb18188debfe7d33b4c4c557b
giordanofaustopedro-alt <giordanofaustopedro@gmail.com>
2026-05-07 19:42:49 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from temperature import is_overheating
 
-temp = float(input("Ingrese temperatura: "))
+try:
+    temp = float(input("Ingrese temperatura: "))
 
-if is_overheating(temp):
-    print("⚠️ ALARMA: Temperatura alta")
-else:
-    print("✅ Temperatura normal")
+    if is_overheating(temp):
+        print("⚠️ ALARMA: Temperatura alta")
+    else:
+        print("✅ Temperatura normal")
+
+except ValueError:
+    print("❌ Entrada inválida")
